97106863ec3b9ae70481a4abfa10de570d5f7a92
Rubel Mahmud <vxlrubel@gmail.com>
2023-12-05 23:15:25 +0600

    update file source

--- inc/Assets.php
+++ inc/Assets.php
@@ -21,22 +21,24 @@ class Assets{
      * @return void
      */
     public function register_frontend_scripts(){
+
         // load style
         wp_enqueue_style(
-            'ci-style',                                    // handle
-            CI_FRONTEND_STYLE,                             // source
-            [],                                            // dependencies
-            CI_VERSION,                                    // version
+            'ci-front-style',           // handle
+            CI_LIB . 'css/style.css',   // source
+            [],                         // dependencies
+            CI_VERSION,                 // version
         );
 
         // load scripts
         wp_enqueue_script( 
-            'ci-script',                                   // handle
-            CI_FRONTEND_SCRIPT,                            // source
-            ['jquery'],                                    // dependencies
-            CI_VERSION,                                    // version
-            true                                           // in_footer
+            'ci-front-script',          // handle
+            CI_LIB . 'js/custom.js',    // source
+            ['jquery'],                 // dependencies
+            CI_VERSION,                 // version
+            true                        // in_footer
         );
+        
     }
 
     /**
@@ -45,22 +47,24 @@ class Assets{
      * @return void
      */
     public function register_admin_scripts(){
+
         // load style
         wp_enqueue_style(
-            'ci-admin-style',    // handle
-            CI_ADMIN_STYLE,      // source
-            [],                  // dependencies
-            CI_VERSION,          // version
+            'ci-admin-style',                // handle
+            CI_ADMIN_LIB . 'css/style.css',  // source
+            [],                              // dependencies
+            CI_VERSION,                      // version
         );
 
         // load scripts
         wp_enqueue_script( 
-            'ci-admin-script',      // handle
-            CI_ADMIN_SCRIPT,        // source
-            ['jquery'],             // dependencies
-            CI_VERSION,             // version
-            true                    // in_footer
+            'ci-admin-script',              // handle
+            CI_ADMIN_LIB . 'js/custom.js',  // source
+            ['jquery'],                     // dependencies
+            CI_VERSION,                     // version
+            true                            // in_footer
         );
+
     }
     
 }
