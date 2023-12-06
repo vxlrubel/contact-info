e63ee16cecb5846d0303d82e1b0eb2dd8a7bfbe7
Rubel Mahmud <vxlrubel@gmail.com>
2023-12-07 00:42:40 +0600

    create admin menu class

---
+++ inc/Admin/Menu.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Contact\Inc\Admin;
+
+defined('ABSPATH') || exit;
+
+
+/**
+ * Create Menu class to manage dashboard menu
+ * 
+ * @version 1.0
+ * @author Rubel Mahmud (Sujan)
+ * @link https://github.com/vxlrubel
+ */
+
+
+class Menu{
+
+    // main slug
+    protected $slug_parent = 'contact-info';
+    
+    public function __construct() {
+        add_action( 'admin_menu', [ $this, 'admin_menu_contact_info' ], 15 );
+    }
+
+    public function admin_menu_contact_info(){
+        add_menu_page( 
+            'Contact Info',                 // page title
+            'Contact Info',                 // menu title
+            'manage_options',               // capability
+            $this->slug_parent,             // menu slug
+            [ $this, '_cb_contact_info' ],  // callback
+            'dashicons-email',              // icon
+            25                              // position
+        );
+    }
+
+    public function _cb_contact_info(){
+        echo 'hello world';
+    }
+}
