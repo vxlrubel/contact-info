db6202ed3e0a8576e8c21b7dfd1b5fe07651a014
Rubel Mahmud <vxlrubel@gmail.com>
2023-12-08 22:22:32 +0600

    create edit form

--- inc/Admin/ContactForm.php
+++ inc/Admin/ContactForm.php
@@ -6,39 +6,48 @@ namespace Contact\Inc\Admin;
 defined('ABSPATH') || exit;
 
 class ContactForm{
-    public function edit_form(){
-        ?>
+    public function edit_form( $edit_id ){
+
+        global $wpdb;
+        
+        $table = $wpdb->prefix . 'contact_info';
+        
+        $sql = "SELECT * FROM $table WHERE id = $edit_id";
+        
+        $get_result = $wpdb->get_results( $sql, ARRAY_A );
+
+        foreach ( $get_result as $result  ):  ?>
 
             <form action="" class="contact-info-form">
 
                 <div class="item-row">
                     <label for="name">Name:</label>
-                    <input type="text" value="name" id="name">
+                    <input type="text" value="<?php echo $result['name']; ?>" id="name">
                 </div>
 
                 <div class="item-row">
                     <label for="email">Email:</label>
-                    <input type="email" value="email" id="email">
+                    <input type="email" value="<?php echo $result['email']; ?>" id="email">
                 </div>
 
                 <div class="item-row">
                     <label for="phone">Phone:</label>
-                    <input type="number" value="phone" id="phone" min="100000000">
+                    <input type="number" value="<?php echo $result['phone']; ?>" id="phone" min="100000000">
                 </div>
 
                 <div class="item-row">
                     <label for="website">Website:</label>
-                    <input type="url" value="website" id="website">
+                    <input type="url" value="<?php echo $result['website']; ?>" id="website">
                 </div>
 
                 <div class="item-row">
                     <label for="address">Address:</label>
-                    <textarea id="address"rows="2"></textarea>
+                    <textarea id="address"rows="2"><?php echo $result['address']; ?></textarea>
                 </div>
 
                 <div class="item-row">
                     <label for="message">Message:</label>
-                    <textarea id="message"rows="5"></textarea>
+                    <textarea id="message"rows="5"><?php echo $result['message']; ?></textarea>
                 </div>
 
                 <div class="item-row">
@@ -47,6 +56,6 @@ class ContactForm{
 
             </form>
 
-        <?php
+        <?php endforeach;
     }
 }
